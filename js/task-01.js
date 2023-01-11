5bf0637d81202b8013f7851c52d6af5e51901e45
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-11 15:47:47 +0200

    part 1
    part 1

--- js/task-01.js
+++ js/task-01.js
@@ -6,7 +6,15 @@ const categoryCount = categoryItems.length;
 
 console.log("Number of categories:", categoryCount);
 
+console.log("");
+
 for (let i = 0; i < categoryCount; i += 1) {
   const categoryTitle = categoryItems[i].firstElementChild.textContent;
   console.log("Category:", categoryTitle);
+
+  const categoryTypeCount =
+    categoryItems[i].lastElementChild.querySelectorAll("li").length;
+  console.log("Elements:", categoryTypeCount);
+
+  console.log("");
 }
