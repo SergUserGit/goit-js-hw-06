e34bba774ff98582dabec262fb957da1e1d730b1
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-11 15:23:20 +0200

    one changes
    one changes

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const categoryList = document.querySelector("#categories");
+
+const countItem = categoryList.querySelectorAll("#categories > li");
+
+console.log("Number of categories:", countItem.length);
