20bd19112d4643551d97917f06083c1fe6e6581d
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-11 16:12:05 +0200

    part 2
    part 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsItems = ingredients.map((el) => {
+  const newItem = document.createElement("li");
+  newItem.textContent = el;
+  newItem.classList.add("item");
+  return newItem;
+});
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredientsList.append(...ingredientsItems);
