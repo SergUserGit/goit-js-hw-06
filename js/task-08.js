ff4ee0edadcc66e700078287506cae93843cd6cb
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-12 14:05:24 +0200

    part 8
    part 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const currtntDate = event.currentTarget;
+  const elemOfForm = currtntDate.elements;
+
+  const email = elemOfForm.email.value;
+  const password = elemOfForm.password.value;
+
+  if (email.trim() === "" || password.trim() === "") {
+    alert("всі поля повинні бути заповнені");
+  } else {
+    const elemObject = { email, password };
+
+    console.log(elemObject);
+
+    currtntDate.reset();
+  }
+}
