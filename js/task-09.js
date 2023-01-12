08afcd35ad2ad0ae468832225b7feddfe06f66be
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-12 14:34:38 +0200

    part 9
    part 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btnChangeColor = document.querySelector(".change-color");
+
+const spanColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
 }
