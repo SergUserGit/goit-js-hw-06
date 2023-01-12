b4cc6904aa13f85d4d27bb40bdf3707e031dbdbc
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-12 15:26:48 +0200

    part 10 begin
    part 10 begin

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+//function getRandomHexColor() {
+//  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const createBtn = document.querySelector("[data-create]");
+
+createBtn.addEventListener("click", onCreateBtnClick);
+
+const destroyBtn = document.querySelector("[data-destroy]");
+destroyBtn.addEventListener("click", onDestroyBtnClick);
+
+function onCreateBtnClick() {
+  const elementInput = document.querySelector("input");
+
+  const inputValue = elementInput.value;
+
+  const inputValueNumber = Number.parseInt(inputValue);
+
+  console.log(!inputValueNumber);
+
+  console.log(typeof inputValueNumber);
+}
+
+function onDestroyBtnClick() {
+  alert("Удаление");
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i <= amount; i += 1) {}
 }
